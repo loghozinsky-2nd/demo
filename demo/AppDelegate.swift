678c6caa39a724545f8e14972c9cd439e068818a
Sasha Loghozinsky <loghozinsky@gmail.com>
2020-09-16 15:11:59 +0300

    Adding files into project

---
+++ demo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  demo
+//
+//  Created by Aleksander Loghozinsky on 11.09.2020.
+//  Copyright © 2020 Aleksander Logozinsky. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ demo/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Aleksander Loghozinsky on 11.09.2020.
+//  Copyright © 2020 Aleksander Logozinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let buttonSecond = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        button.setTitle("MOR3 BUTTON", for: .normal)
+        button.setTitle("FRSB BUTTON", for: .highlighted)
+        
+        buttonSecond.setTitle("dota 3", for: .normal)
+        view.addSubview(buttonSecond)
+        buttonSecond.backgroundColor = .blue
+        buttonSecond.translatesAutoresizingMaskIntoConstraints = false
+        buttonSecond.topAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        buttonSecond.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        buttonSecond.addTarget(self, action: #selector(onDota3Clicked), for: .touchUpInside)
+    }
+    
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        counterLabel.text = String(Int(counterLabel.text!)! + 1)
+    }
+    
+    @objc func onDota3Clicked() {
+        let alert = UIAlertController(title: "Congratulation", message: "Dota 3 has been released", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(action)
+        
+        present(alert, animated: true)
+    }
+    
+}
+
